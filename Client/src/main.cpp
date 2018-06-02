bea125dadecb76b70d8842a3b9fa72b45f940a82
Hubert Korgul <hubi92@wp.pl>
2018-06-02 22:14:36 +0200

    Nieznaczące zmiany

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -15,19 +15,17 @@ int main()
 
 	// game clock
 	float SPEED = 10;
+	int gameTime = 120;
 	sf::Clock clock;
 
 	sf::Clock gameClock;
 	// creating objects
-	int snakesLength = 20;
-	Snake snake({ 5 * FIELD_WIDTH, 5 * FIELD_HEIGHT }, sf::Color(15, 150, 0, 200), snakesLength);
-	Snake snakeOther({ 10 * FIELD_WIDTH, 8 * FIELD_HEIGHT }, sf::Color(240, 0, 0, 200), snakesLength);
-	Snake snakeOther2({ 15 * FIELD_WIDTH, 5 * FIELD_HEIGHT }, sf::Color(0, 120, 255, 200), snakesLength);
-	Snake snakeOther3({ 17 * FIELD_WIDTH, 9 * FIELD_HEIGHT }, sf::Color::Yellow, snakesLength);
-	Snake snakeOther4({ 1 * FIELD_WIDTH, 14 * FIELD_HEIGHT }, sf::Color(160, 160, 160, 200), snakesLength);
+	int snakeLength = 20;
+	Snake snake({ 5 * FIELD_WIDTH, 5 * FIELD_HEIGHT }, sf::Color(15, 150, 0, 200), snakeLength, true);
+	Snake snake2({ 15 * FIELD_WIDTH, 5 * FIELD_HEIGHT }, sf::Color(240, 0, 0, 200), snakeLength, true);
 
-	Snake *players[4] = { &snakeOther, &snakeOther2, &snakeOther3, &snakeOther4 };
-	Scoreboard scoreboard(players, 4, &gameClock);
+	Snake *players[1] = { &snake };
+	Scoreboard scoreboard(players, 1, &gameClock, gameTime);
 
 	Fruit fruit(sf::Vector2i(32, 160));
 	
@@ -73,6 +71,9 @@ int main()
 				case sf::Keyboard::D:
 					snake.setDirection(Snake::Direction::RIGHT);
 					break;
+				case sf::Keyboard::Space:
+					snake.setDirection(Snake::Direction::STOP);
+					break;
 				case sf::Keyboard::Subtract:
 					if (SPEED > 2) SPEED -= 0.5;
 					break;
@@ -81,16 +82,13 @@ int main()
 					break;
 				case sf::Keyboard::R:
 					snake.m_body.deleteAllSegments();
-					snakeOther.m_body.deleteAllSegments();
-					snakeOther2.m_body.deleteAllSegments();
-					snakeOther3.m_body.deleteAllSegments();
-					snakeOther4.m_body.deleteAllSegments();
 					snake.m_fruits = 0;
-					snakeOther.m_fruits = 0;
-					snakeOther2.m_fruits = 0;
-					snakeOther3.m_fruits = 0;
-					snakeOther4.m_fruits = 0;
+					snake2.m_body.deleteAllSegments();
+					snake2.m_fruits = 0;
 					gameClock.restart();
+					scoreboard.setTime(gameTime);
+					snake.m_direction = Snake::Direction::STOP;
+					snake2.m_direction = Snake::Direction::STOP;
 					break;
 				case sf::Keyboard::Escape:
 					window.close();
@@ -101,18 +99,17 @@ int main()
 		}
 
 		// update everything here...
-		if (gameClock.getElapsedTime().asSeconds() > 180) {
-			sf::sleep(sf::seconds(5));
+		scoreboard.update();
+
+		if (scoreboard.timeUp()) {
+			sf::sleep(sf::seconds(3));
 			window.close();
 		}
 
 		// delay
 		if (clock.getElapsedTime() > sf::seconds(1 / SPEED)) {
-			//snake.move(fruit);
-			snakeOther.moveAutomatically(fruit);
-			snakeOther2.moveAutomatically(fruit);
-			snakeOther3.moveAutomatically(fruit);
-			snakeOther4.moveAutomatically(fruit);
+			snake.move(fruit);
+			//snake2.moveAutomatically(fruit);
 			clock.restart();
 		}
 
@@ -121,15 +118,9 @@ int main()
 
 
 		// draw everything here...
-
 		window.draw(fruit);
-		
-		window.draw(snakeOther4);
-		window.draw(snakeOther3);
-		window.draw(snakeOther2);
-		window.draw(snakeOther);
-		//window.draw(snake);
-		
+		//window.draw(snake2);
+		window.draw(snake);	
 		window.draw(scoreboard);
 
 		// end the current frame
