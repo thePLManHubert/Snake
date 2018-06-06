28f7e32556455dd87ebdce95967c0d477611437f
thePLManHubert <hubi92@wp.pl>
2018-06-06 18:49:01 +0200

    Przeniesiono definicję rozmiarów pojedynczego kafelka.

--- Shared/src/Globals.h
+++ Shared/src/Globals.h
@@ -1,4 +1,7 @@
 #pragma once
 
-const int MAP_WIDTH = 32 * 20;
-const int MAP_HEIGHT = 32 * 15;
+const int FIELD_WIDTH = 32;
+const int FIELD_HEIGHT = 32;
+
+const int MAP_WIDTH = FIELD_WIDTH * 20;
+const int MAP_HEIGHT = FIELD_HEIGHT * 15;
