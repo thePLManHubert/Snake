bc3e2f76319fbf490c6c9f2440a1619a6c68b720
thePLManHubert <hubi92@wp.pl>
2018-06-10 18:34:14 +0200

    Możliwy przesył danych przez serwer, łączenie i poprawne rozłączanie klientów.

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -3,10 +3,10 @@
 
 
 void Server::start() {
-	if (m_socket.bind(m_port) != sf::Socket::Done) {
+	if (m_listeningSocket.bind(m_listeningPort) != sf::Socket::Done) {
 		return;
 	}
-	m_socket.setBlocking(false);
+	m_listeningSocket.setBlocking(false);
 	m_listening = true;
 	m_listenThread = std::thread(&Server::listen, this);
 }
@@ -16,12 +16,24 @@ void Server::shutdown() {
 		m_listening = false;
 		m_listenThread.join();
 	}
+	if (m_playing) {
+		m_playing = false;
+		m_gameThread.join();
+	}
 }
 
 void Server::listen() {
 	while (m_listening) {
-		if (m_socket.receive(m_receivedData, MAX_DATA_SIZE, m_receivedSize, m_senderAddress, m_senderPort) == sf::Socket::Done) {
+		if (m_listeningSocket.receive(m_receivedData, MAX_DATA_SIZE, m_receivedSize, m_senderAddress, m_senderPort) == sf::Socket::Done) {
 			process(m_receivedData);
+		}
+	}
+}
+
+void Server::play() {
+	while (m_playing) {	
+		if (m_gameSocket.receive(m_receivedGameData, MAX_DATA_SIZE, m_receivedGameSize, m_playerAddress, m_playerPort) == sf::Socket::Done) {
+			process(m_receivedGameData);
 		}
 	}
 }
@@ -29,37 +41,96 @@ void Server::listen() {
 void Server::process(void* packet) {
 	using namespace Datagram;
 
-	TypeOfPacket type = *(TypeOfPacket*)packet;
+	TypeOfPacket type = checkType(packet);
 
 	switch (type) {
 	case RequestPacket:
-		process(*(Request*)packet);
+		process((Request*)packet);
 		break;
 
 	case DataPacket:
-		process(*(Data*)packet);
+		process((Data*)packet);
+		break;
+
+	case QueuePacket:
+		process((Queue*)packet);
+		break;
+
+	case DCPacket:
+		process((DC*)packet);
 		break;
 	}
 }
 
-void Server::process(Datagram::Request & request) {
+void Server::process(Datagram::Request * request) {
 	using namespace Datagram;
-	m_nPlayers++;
+
 #ifdef DEBUG
-	std::cout << "Otrzymano RequestPacket z " << m_senderAddress << ":" << m_senderPort << std::endl
-			  << "Przydzielono id: " << m_nPlayers << std::endl;
-#endif	
-	if (request.playerID == -1) {
+	std::cout << "Otrzymano RequestPacket z " << m_senderAddress << ":" << m_senderPort << std::endl;
+#endif
+
+	if (m_gamePort == m_listeningPort) {
+		m_gamePort = 5050;
+		m_gameSocket.bind(m_gamePort);
+		m_gameSocket.setBlocking(false);
+		m_playing = true;
+		m_gameThread = std::thread(&Server::play, this);
+	}
+	m_nPlayers++;
+	for (int i = 0; i < 4; i++) {
+		if (!m_players[i]) {
+			m_players[i] = new Player(m_senderAddress, m_senderPort, m_nPlayers);
+#ifdef DEBUG
+			std::cout << "Przydzielono id: " << m_nPlayers << std::endl;
+			std::cout << "Dodano nowego gracza." << std::endl;
+#endif
+			m_playerAdded = true;
+			break;
+		}
+	}
+	
+	if (m_playerAdded && request->playerID == -1) {
+		m_playerAdded = false;
 		Reply reply;
 		reply.playerID = m_nPlayers;
+		reply.newServerPort = m_gamePort;
 		send(&reply, sizeof(Reply), m_senderAddress, m_senderPort);
+	}
+	else {
+		send(request, sizeof(Reply), m_senderAddress, m_senderPort);
+#ifdef DEBUG
+		std::cout << "Brak miejsca na serwerze." << std::endl;
+#endif
 	}
 }
 
-void Server::process(Datagram::Data & data) {
+void Server::process(Datagram::Queue * queue) {
 
 }
 
+void Server::process(Datagram::Data * data) {
+	using namespace Datagram;
+	for (int i = 0; i < 4; i++) {
+		if (m_players[i] && (m_players[i]->id != data->playerID))
+			broadcast(data, sizeof(Data), m_players[i]->ip, m_players[i]->port);
+	}
+}
+
+void Server::process(Datagram::DC * dc) {
+	for (int i = 0; i < 4; i++) {
+		if (m_players[i] && (m_players[i]->id == dc->playerID)) {
+			m_players[i] = nullptr;
+#ifdef DEBUG
+			std::cout << "Rozlaczono gracza o id: " << dc->playerID << std::endl;
+#endif
+		}
+	}
+}
+
 void Server::send(const void * data, size_t size, const sf::IpAddress address, unsigned short port) {
-	m_socket.send(data, size, address, port);
+	m_listeningSocket.send(data, size, address, port);
+}
+
+void Server::broadcast(const void * data, size_t size, const sf::IpAddress address, unsigned short port) {
+	m_gameSocket.send(data, size, address, port);
 }
--- Server/src/Server.h
+++ Server/src/Server.h
@@ -3,38 +3,69 @@
 #include <SFML/Network.hpp>
 #include <thread>
 
+
+struct Player {
+	sf::IpAddress ip;
+	unsigned short port;
+	int id;
+
+	Player(sf::IpAddress ip, unsigned short port, int id)
+		:ip(ip),
+		port(port),
+		id(id)
+	{
+	}
+};
+
 class Server {
 	static const int MAX_DATA_SIZE = 100;
 	static const int MAX_PLAYER_NUMBER = 10;
 
 private:
-	unsigned short m_port;
+	unsigned short m_listeningPort;
+	unsigned short m_gamePort;
 	std::thread m_listenThread;
+	std::thread m_gameThread;
+	volatile bool m_playing;
 	volatile bool m_listening;
-	sf::UdpSocket m_socket;
+	sf::UdpSocket m_listeningSocket;
+	sf::UdpSocket m_gameSocket;
 	size_t m_receivedSize;
+	size_t m_receivedGameSize;
 	char m_receivedData[MAX_DATA_SIZE];
+	char m_receivedGameData[MAX_DATA_SIZE];
 	sf::IpAddress m_senderAddress;
+	sf::IpAddress m_playerAddress;
+	unsigned short m_playerPort;
 	unsigned short m_senderPort;
 	int m_nPlayers;
+	bool m_playerAdded;
+	Player * m_players[4] = {nullptr};
 
 
 public:
 	Server(unsigned short port) : 
-		m_port(port), 
+		m_listeningPort(port), 
+		m_gamePort(port),
 		m_listening(false),
-		m_nPlayers(0)
+		m_playing(false),
+		m_nPlayers(0),
+		m_playerAdded(false)
 	{
 	}
 
 	void start();
 	void shutdown();
 	void send(const void* data, size_t size, const sf::IpAddress address, unsigned short port);
+	void broadcast(const void * data, size_t size, const sf::IpAddress address, unsigned short port);
 	
 private:
 	void listen();
+	void play();
 	void process(void* packet);
-	void process(Datagram::Request & request);
-	void process(Datagram::Data & data);
+	void process(Datagram::Request * request);
+	void process(Datagram::Queue * queue);
+	void process(Datagram::Data * data);
+	void process(Datagram::DC * dc);
 
 };
