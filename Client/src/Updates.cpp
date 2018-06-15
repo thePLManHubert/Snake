5221bffcfa1bf4bfad7515f61137680fe32ae452
thePLManHubert <hubi92@wp.pl>
2018-06-16 00:20:56 +0200

    Uzupełniono tymczasowo funkcje odświeżające dane w grze multiplayer.

--- Client/src/Updates.cpp
+++ Client/src/Updates.cpp
@@ -8,7 +8,7 @@ void Game::updateMenu() {
 /*------------------------------------------------------------------------------------------*/
 
 void Game::updateQueue() {
-
+	m_sprite.setTexture(m_texture);
 }
 
 /*------------------------------------------------------------------------------------------*/
@@ -33,5 +33,18 @@ void Game::updateSingleplayer() {
 /*------------------------------------------------------------------------------------------*/
 
 void Game::updateMultiplayer() {
+	if (!m_snakesPtr) return;
 
+	bool hitFruit = false;
+	hitFruit = m_snakesPtr[0]->move(*m_fruitPtr);
+
+	for (int i = 1; i < m_nPlayers; i++) {
+		hitFruit = m_snakesPtr[i]->move(*m_fruitPtr) || hitFruit;
+	}
+	if (hitFruit)
+		m_fruitPtr->setPosition(m_fruitPtr->preparePosition(*this));
+	if (m_collisionEnabled)
+		detectCollision();
+
+	m_scoreboardPtr->update();
 }
